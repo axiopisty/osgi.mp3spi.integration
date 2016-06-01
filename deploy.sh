e59b2c850c95536d5d2f5c03c0d665ce3ae8f016
Elliot Huntington <elliot.huntington@gmail.com>
2016-05-31 23:51:59 -0600

    static weaving enables java sound api in osgi container.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -x
 mvn clean install
-java -jar com.github.axiopisty.osgi.mp3spi.integration.standalone/target/com.github.axiopisty.osgi.mp3spi.integration.standalone-1.0.0-SNAPSHOT.jar
+java -jar org.apache.aries.spifly.static.tool-1.0.8-jar-with-dependencies.jar com.github.axiopisty.osgi.mp3spi.integration.mp3player/target/com.github.axiopisty.osgi.mp3spi.integration.mp3player-1.0.0-SNAPSHOT.jar
+#java -jar com.github.axiopisty.osgi.mp3spi.integration.standalone/target/com.github.axiopisty.osgi.mp3spi.integration.standalone-1.0.0-SNAPSHOT.jar
 rm -rf felix/felix-cache
 rm felix/bundle/com.github.axiopisty.osgi.mp3spi.integration.*
-cp com.github.axiopisty.osgi.mp3spi.integration.mp3player/target/com.github.axiopisty.osgi.mp3spi.integration.mp3player-1.0.0-SNAPSHOT.jar felix/bundle/
+cp com.github.axiopisty.osgi.mp3spi.integration.mp3player/target/com.github.axiopisty.osgi.mp3spi.integration.mp3player-1.0.0-SNAPSHOT_spifly.jar felix/bundle/
 cp com.github.axiopisty.osgi.mp3spi.integration.bundle.activator/target/com.github.axiopisty.osgi.mp3spi.integration.bundle.activator-1.0.0-SNAPSHOT.jar felix/bundle/
 pushd felix
 java -jar bin/felix.jar
