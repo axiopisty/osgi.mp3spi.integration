e53ebf0e02d43165e1b26cb193ee8564360c53e3
Elliot Huntington <elliot.huntington@gmail.com>
2016-05-31 21:31:50 -0600

    Trying new version of soundlibs.
    These versions are supposedly proper OSGi bundles.

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@ set -x
 mvn clean install
 java -jar com.github.axiopisty.osgi.mp3spi.integration.standalone/target/com.github.axiopisty.osgi.mp3spi.integration.standalone-1.0.0-SNAPSHOT.jar
 rm -rf felix/felix-cache
-rm felix/bundle/com.github.axiopisty.osgi.mp3spi.integration.mp3player-1.0.0-SNAPSHOT.jar
-rm felix/bundle/com.github.axiopisty.osgi.mp3spi.integration.bundle.activator-1.0.0-SNAPSHOT.jar
+rm felix/bundle/com.github.axiopisty.osgi.mp3spi.integration.*
 cp com.github.axiopisty.osgi.mp3spi.integration.mp3player/target/com.github.axiopisty.osgi.mp3spi.integration.mp3player-1.0.0-SNAPSHOT.jar felix/bundle/
 cp com.github.axiopisty.osgi.mp3spi.integration.bundle.activator/target/com.github.axiopisty.osgi.mp3spi.integration.bundle.activator-1.0.0-SNAPSHOT.jar felix/bundle/
 pushd felix
