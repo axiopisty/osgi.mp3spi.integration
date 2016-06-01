d658b7e184111c0d57ea1661069919feabd8b40e
Elliot Huntington <elliot.huntington@gmail.com>
2016-06-01 01:28:56 -0600

    Updated branch documentation.
    Cleaned the branch.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -x
 mvn clean install
 java -jar org.apache.aries.spifly.static.tool-1.0.8-jar-with-dependencies.jar com.github.axiopisty.osgi.mp3spi.integration.mp3player/target/com.github.axiopisty.osgi.mp3spi.integration.mp3player-1.0.0-SNAPSHOT.jar
-#java -jar com.github.axiopisty.osgi.mp3spi.integration.standalone/target/com.github.axiopisty.osgi.mp3spi.integration.standalone-1.0.0-SNAPSHOT.jar
 rm -rf felix/felix-cache
 rm felix/bundle/com.github.axiopisty.osgi.mp3spi.integration.*
 cp com.github.axiopisty.osgi.mp3spi.integration.mp3player/target/com.github.axiopisty.osgi.mp3spi.integration.mp3player-1.0.0-SNAPSHOT_spifly.jar felix/bundle/
